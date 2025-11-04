7eea0d680af91fed00355280729b010cf051a370
joshonline <masanzajoshua@gmail.com>
2025-11-04 06:49:17 -0500

    Basic views added, nav and footer integrated

--- routes/contact.js
+++ routes/contact.js
@@ -2,7 +2,12 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function (req, res, next) {
-  res.render("contact", { title: "Contact Me" });
+  res.render("contact", {
+    title: "Contact Me",
+    Email: "masanzajoshua@gmail.com",
+    GitHub: "https://github.com/joshonline",
+    LinkedIn: "www.linkedin.com/in/joshua-masanza-49499a1b3",
+  });
 });
 
-module.exports = router
+module.exports = router;
