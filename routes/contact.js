1670206d8d2718aa52cbabfff1ba979086e9bb04
joshonline <masanzajoshua@gmail.com>
2025-11-07 10:50:45 -0500

    Added site.js for configuration management, added bootstrap

--- routes/contact.js
+++ routes/contact.js
@@ -1,12 +1,11 @@
 var express = require("express");
 var router = express.Router();
+var siteConfig = require("../public/javascripts/site")
 
 router.get("/", function (req, res, next) {
   res.render("contact", {
     title: "Contact Me",
-    Email: "masanzajoshua@gmail.com",
-    GitHub: "https://github.com/joshonline",
-    LinkedIn: "www.linkedin.com/in/joshua-masanza-49499a1b3",
+    contacts: siteConfig.contacts
   });
 });
 
